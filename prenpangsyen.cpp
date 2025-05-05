c49122d38b8aa5c030382f1776bed6334c7d914d
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-05-05 15:16:45 +0700

    membuat class dosen

--- prenpangsyen.cpp
+++ prenpangsyen.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 using namespace std;
 
+class Dosen {
+    public:
+        string nama;
+        void tampilnama() {
+            cout << "Namanya adalah " << nama << endl;
+        }
+};
+
