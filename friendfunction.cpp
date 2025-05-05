40f17ff60741b33d22c4dbf6dca9bcf079f03117
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-05-05 16:14:41 +0700

    menambahkan class mahasiswa

---
+++ friendfunction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+    private:
+        string nama;
+    public:
+        friend void setnama(mahasiswa &a, string);
+};
+
+void setnama(mahasiswa &a, string b) {
+    a.nama = b;
+    cout << a.nama;
+}
+
