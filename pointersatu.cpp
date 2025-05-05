06e926f9e38b6b1ce314ad1e6c1f2ec85b435ecb
SyahJehanWonopatih <syahjehan00@gmail.com>
2025-05-05 16:18:20 +0700

    menambahkan class mahasiswa dan pointer

---
+++ pointersatu.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+    public:
+        int nim;
+        void shownim() {
+            cout << "No induk = " << nim << endl;
+        }
+};
+
+int main() {
+    mahasiswa mhs;
+    mhs.nim = 5;
+    mhs.shownim();
+
+    mahasiswa &refmhs = mhs;
+    refmhs.nim = 2;
+    mhs.shownim();
+
+    mahasiswa *pmhs = &mhs;
+    pmhs->nim = 3;
+    pmhs->shownim();
+    return 0;
+
+}
